d7605167039345bbdb453eae374d9778bd05a7b5
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-03-31 22:43:12 -0300

    Estiliza pagina de Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { GiWallet } from 'react-icons/gi';
 
 class Login extends Component {
   constructor() {
@@ -30,23 +31,31 @@ class Login extends Component {
     const { email, password, isValidLogin } = this.state;
     const { saveUser } = this.props;
     return (
-      <form>
-        <label htmlFor="email-input">
-          email:
+      <form className="login-form">
+        <div className="logo">
+          <figure>
+            <GiWallet className="icon-wallet" />
+          </figure>
+          <h2 className="title-wallet">TrybeWallet</h2>
+        </div>
+        <label htmlFor="email-input" className="label form-label">
+          email
           <input
             type="text"
             id="email-input"
+            className="input form-input"
             name="email"
             data-testid="email-input"
             value={ email }
             onChange={ this.handleInput }
           />
         </label>
-        <label htmlFor="password-input">
-          senha:
+        <label htmlFor="password-input" className="label form-label">
+          senha
           <input
             type="password"
             id="password-input"
+            className="input form-input"
             name="password"
             data-testid="password-input"
             value={ password }
@@ -55,6 +64,7 @@ class Login extends Component {
         </label>
         <button
           type="button"
+          className="btn btn-form"
           disabled={ !isValidLogin }
           onClick={ () => saveUser(email) }
         >
