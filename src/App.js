fb700e50a97d7c449546c18c279ae507ad30c19b
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-04-01 09:44:16 -0300

    Adiciona componente Header - Requisito 3

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom';
+import { Redirect, Switch } from 'react-router-dom';
 import { Route } from 'react-router-dom/cjs/react-router-dom.min';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App(props) {
   const { isLogged } = props;
   return (
-    <Route exact path="/">
-      {isLogged
-        ? <Redirect to="/carteira" />
-        : <Login />}
-    </Route>
+    <Switch>
+      <Route exact path="/">
+        {isLogged
+          ? <Redirect to="/carteira" />
+          : <Login />}
+      </Route>
+      <Route exact path="/carteira">
+        {isLogged
+          ? <Wallet />
+          : <Redirect to="/" />}
+      </Route>
+    </Switch>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class Header extends Component {
+  render() {
+    const { email } = this.props;
+    return (
+      <header className="header">
+        <h2 data-testid="email-field" className="email-field">{ email }</h2>
+        <div className="header-field">
+          <p data-testid="total-field" className="total-field">{0}</p>
+          <p data-testid="header-currency-field" className="header-currency-field">BRL</p>
+        </div>
+      </header>
+    );
+  }
+}
+
+const mapStateToProps = ({ user: { email } }) => ({ email });
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps, null)(Header);
