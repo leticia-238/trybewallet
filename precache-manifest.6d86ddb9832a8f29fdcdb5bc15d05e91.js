4f5b76ca475b2c60afad438d0428b802afeffeb3
leticia-238 <leticiaoliveiraandrade.9@gmail.com>
2022-11-24 22:46:08 -0300

    Updates

---
+++ precache-manifest.6d86ddb9832a8f29fdcdb5bc15d05e91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17b6ee2aeda1cdb9dd36961beecb6e06",
+    "url": "/wallet/index.html"
+  },
+  {
+    "revision": "69821223063acf2f3cbe",
+    "url": "/wallet/static/css/main.d903d290.chunk.css"
+  },
+  {
+    "revision": "26a97e976dd53eb4fefa",
+    "url": "/wallet/static/js/2.6e546dc3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/wallet/static/js/2.6e546dc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69821223063acf2f3cbe",
+    "url": "/wallet/static/js/main.41051960.chunk.js"
+  },
+  {
+    "revision": "32abb29ede4798555f19",
+    "url": "/wallet/static/js/runtime-main.86db26d2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d580026d18a638d033f24223ddd55058.js"
+  "/wallet/precache-manifest.6d86ddb9832a8f29fdcdb5bc15d05e91.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wallet/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
