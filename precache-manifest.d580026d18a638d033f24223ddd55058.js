dad8b5e879d1848b03f2f9ec8ad6162c74bcbe07
leticia-238 <leticiaoliveiraandrade.9@gmail.com>
2022-11-24 22:10:14 -0300

    Updates

---
+++ precache-manifest.d580026d18a638d033f24223ddd55058.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55d4aa60e9033b00b467f2626a81990b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3baed46141b253422f9e",
+    "url": "/static/css/main.7bb57f66.chunk.css"
+  },
+  {
+    "revision": "26a97e976dd53eb4fefa",
+    "url": "/static/js/2.6e546dc3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6e546dc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3baed46141b253422f9e",
+    "url": "/static/js/main.80bff7f4.chunk.js"
+  },
+  {
+    "revision": "15e2ad1a7730c5f14143",
+    "url": "/static/js/runtime-main.dacb718a.js"
+  }
+]);
